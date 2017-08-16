875c2c0c13b4cea6004be7d3965bd6d7097d53c7
jackyho112 <jackyho112dev@gmail.com>
2017-08-16 12:30:28 -0700

    Add pagination

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { graphql, gql } from 'react-apollo'
 import Link from './Link'
+import { GC_USER_ID, GC_AUTH_TOKEN, LINKS_PER_PAGE } from '../constants'
 
 export const ALL_LINKS_QUERY = gql`
-  query AllLinksQuery {
-    allLinks {
+  query AllLinksQuery($first: Int, $skip: Int, $orderBy: LinkOrderBy) {
+    allLinks(first: $first, skip: $skip, orderBy: $orderBy) {
       id
       createdAt
       url
@@ -19,6 +20,9 @@ export const ALL_LINKS_QUERY = gql`
           id
         }
       }
+    }
+    _allLinksMeta {
+      count
     }
   }
 `
@@ -99,7 +103,7 @@ class LinkList extends Component {
           }
         }
       `,
-      
+
       updateQuery: (previous, { subscriptionData }) => {
         const newAllLinks = [
           subscriptionData.data.Link.node,
@@ -125,6 +129,31 @@ class LinkList extends Component {
     store.writeQuery({ query: ALL_LINKS_QUERY, data })
   }
 
+  getLinksToRender = (isNewPage) => {
+    if (isNewPage) {
+      return this.props.allLinksQuery.allLinks
+    }
+    const rankedLinks = this.props.allLinksQuery.allLinks.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks
+  }
+
+  nextPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page <= this.props.allLinksQuery._allLinksMeta.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1
+      this.props.history.push(`/new/${nextPage}`)
+    }
+  }
+
+  previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page > 1) {
+      const previousPage = page - 1
+      this.props.history.push(`/new/${previousPage}`)
+    }
+  }
+
   componentDidMount() {
     this.subscribeToNewLinks()
     this.subscribeToNewVotes()
@@ -139,25 +168,65 @@ class LinkList extends Component {
     }
 
     if (allLinksQuery && allLinksQuery.error) {
+      console.log(allLinksQuery.error)
       return <div>Error</div>;
     }
 
-    const linksToRender = allLinksQuery.allLinks;
+    const isNewPage = this.props.location.pathname.includes('new')
+    const linksToRender = this.getLinksToRender(isNewPage)
+    const userId = localStorage.getItem(GC_USER_ID)
 
     return (
       <div>
-        {linksToRender.map((link, index) => (
-          <Link
-            key={link.id}
-            index={index}
-            link={link}
-            updateStoreAfterVote={this.updateCacheAfterVote}
-          />
-        ))}
+        {!userId ?
+          <button onClick={() => {
+            this.props.history.push('/login')
+          }}>Login</button> :
+          <div>
+            <button onClick={() => {
+              this.props.history.push('/create')
+            }}>New Post</button>
+            <button onClick={() => {
+              localStorage.removeItem(GC_USER_ID)
+              localStorage.removeItem(GC_AUTH_TOKEN)
+              this.forceUpdate()
+            }}>Logout</button>
+          </div>
+        }
+        <div>
+          {linksToRender.map((link, index) => (
+            <Link
+              key={link.id}
+              updateStoreAfterVote={this._updateCacheAfterVote}
+              link={link}
+              index={index}
+            />
+          ))}
+        </div>
+        {isNewPage &&
+        <div>
+          <button onClick={() => this.previousPage()}>Previous</button>
+          <button onClick={() => this.nextPage()}>Next</button>
+        </div>
+        }
       </div>
     )
   }
 
 }
 
-export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList);
+export default graphql(
+  ALL_LINKS_QUERY, {
+    name: 'allLinksQuery',
+    options: (ownProps) => {
+      const page = parseInt(ownProps.match.params.page, 10)
+      const isNewPage = ownProps.location.pathname.includes('new')
+      const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+      const first = isNewPage ? LINKS_PER_PAGE : 100
+      const orderBy = isNewPage ? 'createdAt_DESC' : null
+      return {
+        variables: { first, skip, orderBy }
+      }
+    }
+  }
+)(LinkList);
